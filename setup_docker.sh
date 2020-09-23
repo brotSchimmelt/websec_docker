9d19d5300a96555d8d5d1ca5fe6027d4da463712
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-09-23 11:15:29 +0200

    add setup script

---
+++ setup_docker.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+printf "\nThis script will set up the .env file for docker and build the container images.\n\n"
+
+printf "Continue? [Y/n]: "
+read answer
+
+if [ -z $answer ]; then
+answer='Y'
+fi
+
+if [ $answer == 'n' ] || [ $answer == 'N' ]; then
+printf "\nOK, bye!\n\n"
+exit 0
+fi
+
+printf "\n\n##### NOTE #####\n"
+printf "For the two DBs ('shop' & 'login') we will configure the same user and password.\n"
+printf "If you wish to change those, you can manually change them in the .env file.\n\n\n"
+
+printf "Removing any old configuration files ...\n\n"
+# make backup files
+mv .env env.backup 2> /dev/null
+mv example.env env.example 2> /dev/null
+rm *.env 2> /dev/null
+
+printf "Enter a username for the MySQL DBs: "
+read user
+
+done_pwd=false
+while ( ! $done_pwd )
+do
+
+printf "\nEnter password for $user: "
+read -s pwd
+
+printf "\nConfirm the password: "
+read -s confirm_pwd
+
+if [ $pwd == $confirm_pwd ]; then
+done_pwd=true
+else
+printf "\n\nThe password does not match. Please try again!"
+fi
+done
+
+done_root=false
+while ( ! $done_root )
+do
+
+printf "\n\nOK, now choose a root password: "
+read -s root_pwd
+
+printf "\nConfirm root password: "
+read -s confirm_root_pwd
+
+if [ $root_pwd == $confirm_root_pwd ]; then
+done_root=true
+else
+printf "\n\nThe password does not match. Please try again!"
+fi
+done
+
+printf "\n\nWriting to '.env' file ...\n\n"
+
+# default values
+php_version="7.3-apache"
+mysql_version="5.7.30"
+timezone="Europe/Berlin"
+
+# content of the configuration file
+env_content="VERSION_PHP=$php_version
+VERSION_MYSQL=$mysql_version
+
+CONTAINER_NAME_PHP=php_apache
+CONTAINER_NAME_MYSQL_SHOP=db_shop
+CONTAINER_NAME_MYSQL_LOGIN=db_login
+CONTAINER_NAME_PMA_SHOP=phpmyadmin_shop
+CONTAINER_NAME_PMA_LOGIN=phpmyadmin_login
+
+MYSQL_DB_SHOP=shop
+MYSQL_USER_SHOP=$user
+MYSQL_PASSWORD_SHOP=$pwd
+MYSQL_ROOT_PASSWORD_SHOP=$root_pwd
+
+MYSQL_DB_LOGIN=login
+MYSQL_USER_LOGIN=$user
+MYSQL_PASSWORD_LOGIN=$pwd
+MYSQL_ROOT_PASSWORD_LOGIN=$root_pwd
+
+TIMEZONE=$timezone"
+
+echo "$env_content" >> .env
+sleep 2
+
+printf "Writing to php config files ...\n\n"
+cp ./www/config/db_login.php ./www/config/db_login.backup
+cp ./www/config/db_shop.php ./www/config/db_shop.backup
+
+sed -i "s!// TODO: change credentials to real ones!!g" ./www/config/db_login.php
+sed -i "s!'DB_USER_LOGIN', '.*'!'DB_USER_LOGIN', '$user'!g" ./www/config/db_login.php
+sed -i "s!'DB_PWD_LOGIN', '.*'!'DB_PWD_LOGIN', '$pwd'!g" ./www/config/db_login.php
+
+sed -i "s!// TODO: change credentials to real ones!!g" ./www/config/db_shop.php
+sed -i "s!'DB_USER_SHOP', '.*'!'DB_USER_SHOP', '$user'!g" ./www/config/db_shop.php
+sed -i "s!'DB_PWD_SHOP', '.*'!'DB_PWD_SHOP', '$pwd'!g" ./www/config/db_shop.php
+
+sleep 2
+
+# check if docker is installed
+docker --version &> /dev/null
+
+if [ $? -gt 0 ]; then
+printf "It seems like 'docker' is not yet installed.\n"
+printf "Please go to https://docs.docker.com/get-docker/ and follow the instructions to install 
+the newest version of docker for your operating system.\n"
+exit 1
+fi
+
+# check if docker-compose is installed
+docker-compose --version &> /dev/null
+
+if [ $? -gt 0 ]; then
+printf "It seems like 'docker-compose' is not yet installed.\n"
+printf "Would you like to install it now from the docker github page? [Y/n] "
+read answer
+
+    if [ -z $answer ]; then
+    answer='Y'
+    fi
+
+    if [ $answer == 'y' ] || [ $answer == 'Y' ]; then
+    printf "\n\nInstalling 'docker-compose' ...\n"
+    sleep 2
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+    # add user to group docker
+    sudo gpasswd -a $USER docker
+    newgrp docker
+
+    sleep 2
+    printf "\n\n"
+    else
+    printf "\nOK, bye!\n\n"
+    exit 0
+    fi
+fi
+
+printf "setting permission for www-data ...\n\n"
+sudo chown www-data ./www/data
+
+printf "building container images (this might take a while) ...\n\n"
+docker-compose build -q
+
+printf "You can now start the server with 'docker-compose up -d'\n\n"
