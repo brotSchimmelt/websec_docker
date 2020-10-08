7aca6c55308daf567456f3c1f8f3270801451053
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-10-08 08:25:55 +0200

    add default administrator user

--- sql/sql_db_login/login.sql
+++ sql/sql_db_login/login.sql
@@ -15,6 +15,11 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `user_name` (`user_name`),
   ADD UNIQUE KEY `user_wwu_email` (`user_wwu_email`);
 
+INSERT INTO `users`
+(`user_id`, `user_name`, `user_pwd_hash`, `user_wwu_email`, `is_unlocked`, `is_admin`, `timestamp`, `last_login`)
+VALUES (1, 'administrator', '$2y$13$X5v4r.pNv0USvF0k4RENPe4zEAuidzOBsY4Sr29h7kBjvMSR3B8Mm', 
+'tim.knebler@uni-muenster.de', 1, 1, '1970-01-01 00:00:01', NULL);
+
 CREATE TABLE `fakeCookie` (
   `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL COMMENT 'auto incrementing id of each user, unique index',
   `user_name` varchar(64) NOT NULL COLLATE utf8mb4_unicode_ci COMMENT 'user name, unique',
@@ -25,6 +30,10 @@ CREATE TABLE `fakeCookie` (
 
 ALTER TABLE `fakeCookie`
   ADD UNIQUE KEY `user_name` (`user_name`);
+
+INSERT INTO `fakeCookie`
+(`id`, `user_name`, `reflective_xss`, `stored_xss`, `fake_token`) VALUES
+(1, 'administrator', 'cc80d244b6ca403f', '24ee09f64e93a128', 'cb3dedbefd49a997');
 
 CREATE TABLE `challengeStatus` (
   `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL COMMENT 'auto incrementing id of each user, unique index',
@@ -43,6 +52,10 @@ CREATE TABLE `challengeStatus` (
 
 ALTER TABLE `challengeStatus`
   ADD UNIQUE KEY `user_name` (`user_name`);
+
+INSERT INTO `challengeStatus`
+(`id`, `user_name`, `reflective_xss`, `stored_xss`, `sqli`, `csrf`, `csrf_referrer`, `reflective_xss_hard`, `stored_xss_hard`, `sqli_hard`, `csrf_hard`, `csrf_referrer_hard`) 
+VALUES (1, 'administrator', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 CREATE TABLE `resetPwd` (
   `request_id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL COMMENT 'auto incrementing id of each request, unique index',
