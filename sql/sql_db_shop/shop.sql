cdfa8ce3b24fa6415ca0876e5acd394bc8548bf5
brotSchimmelt <brotschimmelt+git@gmail.com>
2020-10-22 00:02:49 +0200

    add new mysql charset

--- sql/sql_db_shop/shop.sql
+++ sql/sql_db_shop/shop.sql
@@ -2,11 +2,11 @@ USE `shop`;
 
 CREATE TABLE `products` (
   `prod_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'auto incrementing id of each product, unique index',
-  `prod_title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'product title',
-  `prod_description` text COLLATE utf8mb4_general_ci NOT NULL COMMENT 'some description text for the product',
+  `prod_title` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'product title',
+  `prod_description` text COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'some description text for the product',
   `price` int(11) NOT NULL DEFAULT '42' COMMENT 'well, the price',
-  `img_path` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'path to product image'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='fake product data';
+  `img_path` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'path to product image'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs COMMENT='fake product data';
 
 INSERT INTO `products` (`prod_id`, `prod_title`, `prod_description`, `price`, `img_path`) VALUES
 (1, 'WebSec Mug', 'Some quick example text to build on the card title and make up the bulk of the cards content.', 100, '/assets/img/prod/prod_mug.jpg'),
@@ -25,7 +25,7 @@ CREATE TABLE `cart` (
   `user_name` varchar(64) NOT NULL COMMENT 'corresponding user name, unique with prod_id',
   `quantity` int(11) NOT NULL COMMENT 'quantity',
   `timestamp` datetime NOT NULL COMMENT 'timestamp of creation'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='shopping cart data';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs COMMENT='shopping cart data';
 
 ALTER TABLE `cart`
   ADD UNIQUE KEY `unique_key` (`prod_id`,`user_name`);
@@ -33,18 +33,18 @@ ALTER TABLE `cart`
 
 CREATE TABLE `xss_comments` (
   `comment_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'auto incrementing id of each comment, unique index',
-  `author` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'username of comment author',
-  `text` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'comment body',
+  `author` varchar(64) COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'username of comment author',
+  `text` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'comment body',
   `rating` int(11) NOT NULL COMMENT 'rating for products',
   `timestamp` datetime NOT NULL COMMENT 'timestamp of creation',
-  `post_time` varchar(64) COLLATE utf8mb4_general_ci DEFAULT 'just now' COMMENT 'time of posting'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='comments for the stored xss challenge';
+  `post_time` varchar(64) COLLATE utf8mb4_0900_as_cs DEFAULT 'just now' COMMENT 'time of posting'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs COMMENT='comments for the stored xss challenge';
 
 
 CREATE TABLE `csrf_posts` (
   `post_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'auto incrementing id of each post, unique index',
-  `user_name` varchar(64) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'username of crosspost author',
-  `message` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'post body',
-  `referrer` varchar(255) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'source of post',
+  `user_name` varchar(64) COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'username of crosspost author',
+  `message` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'post body',
+  `referrer` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL COMMENT 'source of post',
   `timestamp` datetime NOT NULL COMMENT 'timestamp of creation'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='posts for the csrf challenge';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs COMMENT='posts for the csrf challenge';
